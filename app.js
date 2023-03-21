890c839b81d9ad4e0a0573a7703a2d6d695e25ee
EGYuda23 <egyyudanugraha@gmail.com>
2023-03-21 20:50:04 +0700

    Change name

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send(`Hallo! I'm Egy Yuda Nugraha, <a href="https://egyyudanugraha.site">see my portofolio</a>`)
 })
 
 app.listen(PORT, HOST)
